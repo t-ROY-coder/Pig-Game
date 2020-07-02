410986d3a08d2d7e42bfca312e35c9d71c86f95a
TANUMON ROY <roytanumon@gmail.com>
2020-07-03 00:50:18 +0530

    game-over update

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 */
 
-var score, roundScore, activePlayer
+var score, roundScore, activePlayer, isGameOn
 newGame()
 
 function togglePlayer() {
@@ -22,41 +22,55 @@ function newGame() {
     roundScore = 0
     activePlayer = 0
     document.querySelector('.dice').style.display = 'none'
+    isGameOn = true
 
     document.getElementById('score-0').textContent = '0'
     document.getElementById('score-1').textContent = '0'
     document.getElementById('current-0').textContent = '0'
     document.getElementById('current-1').textContent = '0'
+    document.querySelector('#name-0').textContent = 'Player-1'
+    document.querySelector('#name-1').textContent = 'Player-2'
+    document.querySelector('.player-0-panel').classList.remove('winner')
+    document.querySelector('.player-1-panel').classList.remove('winner')
+    document.querySelector('.player-0-panel').classList.remove('active')
+    document.querySelector('.player-1-panel').classList.remove('active')
+    document.querySelector('.player-0-panel').classList.add('active')
+
 }
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
-    var dice = Math.floor(Math.random() * 6) + 1
-    var diceDOM = document.querySelector('.dice')
-    diceDOM.style.display = 'block'
-    diceDOM.src = 'dice-' + dice + '.png'
-    if (dice > 1) {
-        roundScore += dice
-        document.querySelector('#current-' + activePlayer).textContent = roundScore
-    } else {
-        roundScore = 0
-        document.querySelector('#current-' + activePlayer).textContent = roundScore
-        togglePlayer()
-    }
+    if (isGameOn) {
 
+        var dice = Math.floor(Math.random() * 6) + 1
+        var diceDOM = document.querySelector('.dice')
+        diceDOM.style.display = 'block'
+        diceDOM.src = 'dice-' + dice + '.png'
+        if (dice > 1) {
+            roundScore += dice
+            document.querySelector('#current-' + activePlayer).textContent = roundScore
+        } else {
+            roundScore = 0
+            document.querySelector('#current-' + activePlayer).textContent = roundScore
+            togglePlayer()
+        }
+    }
 })
 
 document.querySelector('.btn-hold').addEventListener('click', function() {
-    score[activePlayer] += roundScore
-    roundScore = 0
-    document.querySelector('#score-' + activePlayer).textContent = score[activePlayer]
-    document.querySelector('#current-' + activePlayer).textContent = roundScore
-    if (score[activePlayer] >= 20) {
-        document.querySelector('#name-' + activePlayer).textContent = 'WINNER!'
-        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner')
-        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active')
-        document.querySelector('.dice').style.display = 'none'
-    } else {
-        togglePlayer()
+    if (isGameOn) {
+        score[activePlayer] += roundScore
+        roundScore = 0
+        document.querySelector('#score-' + activePlayer).textContent = score[activePlayer]
+        document.querySelector('#current-' + activePlayer).textContent = roundScore
+        if (score[activePlayer] >= 20) {
+            isGameOn = false
+            document.querySelector('#name-' + activePlayer).textContent = 'WINNER!'
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner')
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active')
+            document.querySelector('.dice').style.display = 'none'
+        } else {
+            togglePlayer()
+        }
     }
 })
 
